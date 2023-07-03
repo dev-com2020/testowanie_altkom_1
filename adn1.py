35380769f9bc01abf27a4f89f796eb13443a5e04
dev-com2020 <kwp@tlen.pl>
2023-07-03 11:27:52 +0200

    11:27 03.07

--- adn1.py
+++ adn1.py
@@ -8,6 +8,26 @@ def greetings():
     print("WITAJ PONOWNIE")
 
 
+@pytest.fixture(scope="class")
+def provide_current_time(request):
+    import datetime
+    request.cls.now = datetime.datetime.now()
+    print("WEJŚCIE DO KLASY")
+    yield
+    print("OPUSZCZENIE KLASY")
+
+
+@pytest.mark.usefixtures("provide_current_time")
+class TestClass:
+    def test_first(self):
+        print("DATA ROZPOCZĘCIA", self.now)
+        assert 1 == 1
+
+    @pytest.mark.usefixtures("greetings")
+    def test_second(self):
+        assert 2 == 2
+
+
 class TestCase:
     def test_second(self):
         assert 10 == 10
